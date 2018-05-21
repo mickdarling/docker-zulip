mickdarling/docker-zulip

commit 4a9d6d34c48b2c4632b1760ea9c74da7d3dad24d

Use zulip/zulip-postgresql for the database
With this change the zulip specific database initialization moves out
of the zulip container and into the database container.

docker-compose.yml
@@ -1,7 +1,7 @@
 version: '2'
 services:
   database:
-    image: "quay.io/galexrt/postgres-zulip-tsearchextras:latest"
+    image: "zulip/zulip-postgresql"
     environment:
       POSTGRES_DB: zulip
       POSTGRES_USER: zulip

entrypoint.sh
@@ -382,26 +382,6 @@ waitingForDatabase() {
     done
     unset PGPASSWORD
 }
-bootstrapDatabase() {
-    echo "(Re)creating database structure ..."
-    if [ ! -z "$DB_ROOT_USER" ] && [ ! -z "$DB_ROOT_PASS" ]; then
-        echo "Setting up the database, schema and user ..."
-        export PGPASSWORD="$DB_ROOT_PASS"
-        echo """
-        CREATE USER $DB_USER;
-        ALTER ROLE $DB_USER SET search_path TO $DB_NAME,public;
-        CREATE DATABASE $DB_NAME OWNER=$DB_USER;
-        CREATE SCHEMA $DB_SCHEMA AUTHORIZATION $DB_USER;
-        """ | psql -h "$DB_HOST" -p "$DB_HOST_PORT" -U "$DB_USER" || :
-        echo "Creating tsearch_extras extension ..."
-        echo "CREATE EXTENSION tsearch_extras SCHEMA $DB_SCHEMA;" | \
-        psql -h "$DB_HOST" -p "$DB_HOST_PORT" -U "$DB_ROOT_USER" "$DB_NAME" || :
-        unset PGPASSWORD
-        echo "Database structure recreated."
-    else
-        echo "No database root user nor password given. Not (re)creating database structure."
-    fi
-}
 bootstrapRabbitMQ() {
     echo "Bootstrapping RabbitMQ ..."
     set +e
@@ -479,7 +459,6 @@ runPostSetupScripts() {
 bootstrappingEnvironment() {
     echo "=== Begin Bootstrap Phase ==="
     waitingForDatabase
-    bootstrapDatabase
     bootstrapRabbitMQ
     zulipFirstStartInit
     zulipMigration

kubernetes/zulip-rc.yml
@@ -43,7 +43,7 @@ spec:
         - name: RABBITMQ_DEFAULT_PASS
           value: "zulip"
       - name: postgresql
-        image: quay.io/galexrt/zulip-postgresql-tsearchextras:latest
+        image: zulip/zulip-postgresql
         resources:
           limits:
             cpu: 80m

commit 752a496fdfb28c998b231ad1e0fa83af38b98b8d

postgres: Fix configuration for getting postgres password.
This fixes a regression introduced in 4a9d6d34c48b2c4632b1760ea9c74da7d3dad24d.

docker-compose.yml
@@ -35,13 +35,13 @@ services:
       DB_HOST: "database"
       DB_HOST_PORT: "5432"
       DB_USER: "zulip"
-      DB_PASS: "zulip"
       SSL_CERTIFICATE_GENERATION: "self-signed"
       SETTING_MEMCACHED_LOCATION: "memcached:11211"
       SETTING_RABBITMQ_HOST: "rabbitmq"
       SETTING_REDIS_HOST: "redis"
       SECRETS_email_password: "123456789"
       SECRETS_rabbitmq_password: "zulip"
+      SECRETS_postgres_password: "zulip"
       SECRETS_secret_key: "REPLACE_WITH_SECURE_SECRET_KEY"
       SETTING_EXTERNAL_HOST: "example.com"
       SETTING_ZULIP_ADMINISTRATOR: "admin@example.com"

entrypoint.sh
@@ -12,7 +12,6 @@ DB_HOST_PORT="${DB_HOST_PORT:-5432}"
 DB_NAME="${DB_NAME:-zulip}"
 DB_SCHEMA="${DB_SCHEMA:-zulip}"
 DB_USER="${DB_USER:-zulip}"
-DB_PASS="${DB_PASS:-zulip}"
 DB_ROOT_USER="${DB_ROOT_USER:-postgres}"
 DB_ROOT_PASS="${DB_ROOT_PASS:-$(echo $DB_PASS)}"
 REMOTE_POSTGRES_SSLMODE="${REMOTE_POSTGRES_SSLMODE:-prefer}"
@@ -329,7 +328,7 @@ initialConfiguration() {
 }
 # === bootstrappingEnvironment ===
 waitingForDatabase() {
-    export PGPASSWORD="$DB_PASS"
+    export PGPASSWORD="$SECRETS_postgres_password"
     local TIMEOUT=60
     echo "Waiting for database server to allow connections ..."
     while ! /usr/bin/pg_isready -h "$DB_HOST" -p "$DB_HOST_PORT" -U "$DB_USER" -t 1 >/dev/null 2>&1

kubernetes/zulip-rc.yml
@@ -100,6 +100,8 @@ spec:
           value: "123456789"
         - name: SECRETS_secret_key
           value: "REPLCAE_WITH_SECURE_SECRET_KEY"
+        - name: SECRETS_postgrse_password
+          value: "zulip"
         - name: SSL_CERTIFICATE_GENERATION
           value: "self-signed"
         ports: